ef9c47af923dac461ac5f3c5224029067081b105
Alex Long <shiversandmilk@gmail.com>
2016-06-20 22:42:09 -0400

    Don't add jitter if creature's position hasn't changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Motion, spring} from 'react-motion';
+import _ from 'lodash';
 var json = require("json!./creatures.json");
 
 
@@ -19,6 +20,16 @@ const App = React.createClass({
         if (xhttp.readyState == 4 && xhttp.status == 200) {
           var newCreatures = JSON.parse(xhttp.responseText);
           self.setState({creatures: newCreatures.map(newCreature => {
+            const oldCreature = _.head(self.state.creatures.filter(c => c.id === newCreature.id)) || {};
+            if (newCreature.loc !== oldCreature.loc) {
+              newCreature.moved = true;
+            } else {
+              newCreature.moved = false;
+              newCreature.xJitter = oldCreature.xJitter;
+              newCreature.yJitter = oldCreature.yJitter;
+            }
+
+
             newCreature.x = newCreature.loc % 3;
             newCreature.y = (newCreature.loc - newCreature.x) / 3;
             return newCreature;
@@ -27,7 +38,7 @@ const App = React.createClass({
       };
       xhttp.open("GET", "http://localhost:8888", true);
       xhttp.send();
-    }, 2000);
+    }, 500);
   },
 
   handleMouseDown() {
@@ -43,7 +54,6 @@ const App = React.createClass({
     const randY = Math.floor(Math.random() * 50) + 1;
     const zeroStyle = {x: spring(0), y: spring(0)};
     const style = {x: spring(randX), y: spring(randY)};
-    console.log(this.state.creatures);
     return (
         <div>
           <div>
@@ -51,21 +61,29 @@ const App = React.createClass({
               Toggle
             </button>
           </div>
-          {this.state.creatures.map(creature => {
-            return <Motion style={{x: spring(creature.x * 300 + this.getJitter()), y: spring(creature.y * 300 + this.getJitter())}}>
-              {({x, y}) => {
-                // children is a callback which should accept the current value of
-                // `style`
-                return (
-                  <div className="creature" style={{
-                    position: "absolute",
-                    transform: `translate3d(${x}px, ${y}px, 0)`,
-                  }}>{creature.id}</div>
-                  );
-                }
+          {
+            this.state.creatures.map(creature => {
+
+              if (creature.moved || creature.xJitter == null) {
+                creature.xJitter = this.getJitter();
+                creature.yJitter = this.getJitter();
               }
-            </Motion>
-          })}
+              return <Motion style={{x: spring(creature.x * 300 + creature.xJitter), y: spring(creature.y * 300 + creature.yJitter)}} key={"x" + creature.id}>
+                {({x, y}) => {
+                  // children is a callback which should accept the current value of
+                  // `style`
+                  return (
+                    <div className="creature"
+                      style={{
+                        position: "absolute",
+                        transform: `translate3d(${x}px, ${y}px, 0)`,
+                      }}>{creature.id}</div>
+                    );
+                  }
+                }
+              </Motion>
+            })
+          }
         </div>
     );
   }
